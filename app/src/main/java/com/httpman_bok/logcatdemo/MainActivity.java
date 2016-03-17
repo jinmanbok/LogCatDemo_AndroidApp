2b85df651f836c0fbfbce5eed8fc6c8fd60d6bcb
jinmanbok <jinmanbok518@hotmail.com>
2016-03-17 12:42:12 -0700

    Initial commit

---
+++ app/src/main/java/com/httpman_bok/logcatdemo/MainActivity.java
@@ -0,0 +1,40 @@
+package com.httpman_bok.logcatdemo;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+
+public class MainActivity extends Activity {
+      private static final String TAG = MainActivity.class.getSimpleName();
+
+
+    String name = "dhddh";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        Log.d(TAG, name);
+
+        Log.i(TAG, "Inside OnCreate Method");
+        Log.e(TAG, "Inside OnCreate Method");
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+
+}
